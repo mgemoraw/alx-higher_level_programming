b8b1ed75fec9b42a45a8884cd8d2b17cc2a8905a
mgemoraw <meng.get4@gmail.com>
2023-03-08 00:49:53 +0300

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for c in [[123:96:1], [90:64:1]]:
+    print("{0}".format(chr(c)))
---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    nstr = ""
+    for i in rage(len(str)):
+        if (i == n):
+            continue;
+        else:
+            nstr += str[i]
