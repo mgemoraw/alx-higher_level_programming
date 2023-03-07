ecd11e9d4cf923d18bf0af1874778b535541ae59
mgemoraw <meng.get4@gmail.com>
2023-03-07 23:38:45 +0300

    00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if ((j != 0) and (i != 9 and j != 9)):
-            print("{0}{1}".format(i, j), end=", ")
-print("{0}{1}".format(9, 9))
+for i in range(100):
+    if (i < 99):
+        print("{:02d}".format(i), end=", ")
+print("{0}".format(99))
