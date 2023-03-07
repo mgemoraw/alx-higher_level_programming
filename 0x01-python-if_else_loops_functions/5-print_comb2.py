ba5c95477ca345462bad65ee3fd775cec4a85a1f
mgemoraw <meng.get4@gmail.com>
2023-03-07 23:32:56 +0300

    minor fixes

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{0}".format(i), end=", ")
-print(99)
+for i in range(10):
+    for j in range(10):
+        if ((j != 0) and (i != 9 and j != 9)):
+            print("{0}{1}".format(i, j), end=", ")
+print("{0}{1}".format(9, 9))
