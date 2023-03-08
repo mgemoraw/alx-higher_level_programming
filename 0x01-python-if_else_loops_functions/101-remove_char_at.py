65dc942966645db061656eafc3ac27365dd946ca
mgemoraw <meng.get4@gmail.com>
2023-03-08 13:10:12 +0300

    remove string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -6,5 +6,5 @@ def remove_char_at(str, n):
             continue;
         else:
             nstr += str[i]
-    print("{0}".format(nstr))
-    return (0);
+    # return ("{0}".format(nstr))
+    return (nstr);
