b962877dc549692f438b386b05a387ad7c7601da
mgemoraw <meng.get4@gmail.com>
2023-03-10 05:11:18 +0300

    hidden discoveries

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """Hidden discovery"""
+
     import hidden_4
 
     names = dir(hidden_4)
-    
+
     for name in names:
         if name[:2] != '__':
             print("{}".format(name))
