a8440ffb7b02fcb914bff1d8fe15faca8364ba14
mgemoraw <meng.get4@gmail.com>
2023-03-10 04:51:18 +0300

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,10 +4,11 @@ if __name__ == "__main__":
 
     n = len(sys.argv) - 1
 
-    sum = 0
+    asum = 0
+
     if (n > 0):
         for x in range(1, n + 1):
-            sum += int(sys.argv[x])
+            asum += int(sys.argv[x])
 
-    print("{}".format(sum))
+    print("{}".format(asum))
 
