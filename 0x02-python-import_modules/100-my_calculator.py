0652dda0e1f2b7a6fc1ba751bc9d2d4e0de450de
mgemoraw <meng.get4@gmail.com>
2023-03-10 05:51:28 +0300

    my last commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,15 +2,15 @@
 if __name__ == "__main__":
     """My personal calculator programe"""
 
-    import sys
     from calculator_1 import add, sub, mul, div
+    import sys
 
     n = len(sys.argv) - 1
 
     ops = {'+':add, '-':sub, '*':mul, '/':div}
 
     if (n != 3):
-        print("Usage: {} <a> <operator> <b>".format(sys.argv[0]))
+        print("Usage: ./{} <a> <operator> <b>".format(sys.argv[0]))
     
     if (sys.argv[2] not in list(ops.keys())):
         print("Unknown operator. Available operrators: +, -, * and /")
