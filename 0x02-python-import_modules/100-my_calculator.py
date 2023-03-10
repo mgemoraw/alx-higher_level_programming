03869ab977e37bd6423bee940485e141c8ff7231
mgemoraw <meng.get4@gmail.com>
2023-03-10 05:58:29 +0300

    Hello me

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,10 +7,11 @@ if __name__ == "__main__":
 
     n = len(sys.argv) - 1
 
-    ops = {'+':add, '-':sub, '*':mul, '/':div}
+    ops = {'+':add, '-':sub, '*':mul, '/':div, ';':''}
 
     if (n != 3):
         print("Usage: ./{} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
     
     if (sys.argv[2] not in list(ops.keys())):
         print("Unknown operator. Available operrators: +, -, * and /")
