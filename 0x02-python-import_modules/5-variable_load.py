6c4aeb74bcd2198899ad42dae8cb595b1f8a5b7e
mgemoraw <sgetme@outlook.com>
2023-03-09 18:03:32 -0800

    Everything can be imported

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -2,9 +2,8 @@
 if __name__ == "__main__":
     """print 1 and 2"""
 
-    from add_0 import add
+    from variable_load_5 import a
 
-    a = 1
-    b = 2
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{}".format(a))
+
