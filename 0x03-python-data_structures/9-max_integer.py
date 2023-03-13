c10cd35d81dcd254093f9aeb218b3557f44d9a36
mgemoraw <meng.get4@gmail.com>
2023-03-13 23:21:02 +0300

    Find max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
 
+def max_integer(my_list = []):
+    if len(my_list) == 0:
+        return (None)
+
+    get_max = my_list[0]
+    for i in range(0, len(my_list)):
+        if get_max < my_list[i]:
+            get_max = my_list[i]
+        else:
+            continue
+    return (get_max)
