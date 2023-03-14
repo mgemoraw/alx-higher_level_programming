9512a010b25ba9132c98fb2c9d55e64c962defdf
mgemoraw <meng.get4@gmail.com>
2023-03-15 00:36:46 +0300

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -6,5 +6,5 @@ def no_c(my_string):
         if my_string[i] in "cC":
             continue
         else:
-            new_str += my_string[i];
+            new_str += my_string[i]
     return new_str
