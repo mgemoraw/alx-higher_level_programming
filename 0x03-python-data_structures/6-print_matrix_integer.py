eedcc01da1643b6c19636706f98f537e98e62187
mgemoraw <meng.get4@gmail.com>
2023-03-14 08:20:54 +0300

    Print a list of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 
+def print_matrix_integer(matrix=[[]]):
+    for row in range(0, len(matrix)):
+        for col in range(0, len(matrix[row])):
+            print("{}".format(matrix[row][col]), end=" ")
+
+        print("{}".format(matrix[row][col]) + "$")
+
+    for row in matrix:
+        for col in row and matrix.index(row) < len(row):
+            print("{}".format(matrix[row][col]), end=" ")
+            
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
