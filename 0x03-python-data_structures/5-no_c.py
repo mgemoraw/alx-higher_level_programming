c69e0ccb0372c1e9436e6bd3ab352e7024b2195a
mgemoraw <meng.get4@gmail.com>
2023-03-15 00:35:34 +0300

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # function removed c or C
 def no_c(my_string):
-	new_str = ""
-	for i in range(len(my_string)):
-		if my_string[i] in "cC":
-			continue
-		else:
-			new_str += my_string[i];
-	return new_str
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] in "cC":
+            continue
+        else:
+            new_str += my_string[i];
+    return new_str
