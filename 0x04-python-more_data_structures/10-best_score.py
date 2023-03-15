9f7e97c64465e793dc82f12787f102af5ebdeecb
mgemoraw <meng.get4@gmail.com>
2023-03-15 23:17:32 +0300

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,10 +7,3 @@ def best_score(a_dictionary):
     for key, value in a_dictionary.items():
         if value == max_value:
             return key
-
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
-print("Best score: {}".format(best_key))
-
-best_key = best_score(None)
-print("Best score: {}".format(best_key))
