26b0a26a01d3d732c542e2dc56dc5476c23370cd
mgemoraw <meng.get4@gmail.com>
2023-03-15 23:35:07 +0300

    Best Score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return None
+
     max_value = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if value == max_value:
