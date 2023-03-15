07c6f01ba9c322229383fa45012451584197e25e
mgemoraw <meng.get4@gmail.com>
2023-03-15 23:34:00 +0300

    Roman Integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-
     max_value = max(a_dictionary.values())
     for key, value in a_dictionary.items():
         if value == max_value:
--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M':, 1000}
+    """
+      :type roman_string: str
+      :Return: integer
+      """
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000, 'IV': 4, 'IX': 9, 'XL': 40, 'XC': 90, 'CD': 400, 'CM': 900}
     if roman_string is None:
         return (0)
-    
+
+    i, num = 0, 0
+
+    while (i < len(roman_string)):
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in romans:
+            num += romans[roman_string[i : i + 2]]
+            i+=2
+        else:
+            num += romans[roman_string[i]]
+            i+= 1
