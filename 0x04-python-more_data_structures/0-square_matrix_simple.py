07543aefc4e6fa8f7b7252ceac78ec850d55768e
mgemoraw <meng.get4@gmail.com>
2023-03-15 21:15:09 +0300

    Squared Sample

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	new_matrix = []
-	for row in matrix:
-		new_matrix.append([i**2 for i in row])
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([i**2 for i in row])
 
-	return new_matrix
-	
+    return new_matrix
+    
