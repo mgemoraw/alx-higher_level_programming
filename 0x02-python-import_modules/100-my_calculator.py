600ea2a50ef416984201b2a2f4997851cd3fcca3
mgemoraw <meng.get4@gmail.com>
2023-03-16 00:28:20 +0300

    Build my own alculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """My personal calculator programe"""
-
+    """My Calculator functions."""
     from calculator_1 import add, sub, mul, div
     import sys
 
-    n = len(sys.argv) - 1
-
-    ops = {'+':add, '-':sub, '*':mul, '/':div}
-
-    if (n != 3):
-        print("Usage: ./{} <a> <operator> <b>".format(sys.argv[0]))
-        sys.exit(1)
-    
-    if (sys.argv[2] not in list(ops.keys())):
-        print("Unknown operator. Available operrators: +, -, * and /")
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a, b = int(sys.argv[1]), int(sys.argv[3])
-    
-    print("{} {} {} = {}".format(a,sys.argv[2], b, ops[sys.argv[2]](a, b)))
-    sys.exit(0)
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
