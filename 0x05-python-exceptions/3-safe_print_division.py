c9bb4e850c47426050e871932a42df91618f03eb
mgemoraw <meng.get4@gmail.com>
2023-03-27 14:46:50 +0300

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    result = 0
+    try:
+        result = a / b
+    
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("{}".format(result))
+    return (result)
+
+
+"""TEST CODE
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+"""
