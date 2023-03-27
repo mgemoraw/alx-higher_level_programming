8b63d89818680435f3340a325d8d0fba46887798
mgemoraw <meng.get4@gmail.com>
2023-03-27 22:16:02 +0300

    Raise a message

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError (message)
+
+
+"""TEST CODE
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
+
+"""
