4aed5abc4c2b5fa268fa0706305d8b8e99346417
mgemoraw <meng.get4@gmail.com>
2023-03-27 12:31:19 +0300

    safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(0, x):
+            print("{:d}".format(my_list[i]), end="") 
+        return x
+    except:
+        raise Exception(x)
