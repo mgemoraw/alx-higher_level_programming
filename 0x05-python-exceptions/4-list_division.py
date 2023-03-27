096067fb143237679efb204bd557994d5e876157
mgemoraw <meng.get4@gmail.com>
2023-03-27 22:52:53 +0300

    List division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -19,7 +19,7 @@ def list_division(my_list_1, my_list_2, list_length):
     return (length)
 
 
-# """TEST CODE
+"""TEST CODE
 my_l_1 = [10, 8, 4]
 my_l_2 = [2, 4, 4]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
@@ -32,4 +32,4 @@ my_l_2 = [2, 0, "H", 2, 7]
 result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
 print(result)
 
-# """
+"""
