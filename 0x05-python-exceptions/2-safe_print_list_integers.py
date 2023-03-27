c40958019cbb4645b0996d39250448be5414f0d1
mgemoraw <meng.get4@gmail.com>
2023-03-27 14:41:08 +0300

    print and count integers

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -15,7 +15,7 @@ def safe_print_list_integers(my_list=[], x=0):
     return (count)
 
 
-# """TEST CODE
+"""TEST CODE
 my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list_integers(my_list, 2)
@@ -28,4 +28,4 @@ print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
 
-# """
+"""
