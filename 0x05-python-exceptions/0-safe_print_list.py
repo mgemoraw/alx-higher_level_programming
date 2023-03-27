f41fa2d17433303f46f24214470182bed021f361
mgemoraw <meng.get4@gmail.com>
2023-03-27 12:42:33 +0300

    Safe list printin

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
+    for i in range(x):
         try:
-            print("{}".format(my_list[i]), end="") 
-        except:
+            print("{}".format(my_list[i]), end="")
+        except Exception:
             pass
- 
     print()
     return (x)
