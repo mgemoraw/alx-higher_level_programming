cafb5f9834bfa58be924a5bb24a7a77a560bd8f7
mgemoraw <meng.get4@gmail.com>
2023-03-28 22:33:20 +0300

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+# Square = __import__('0-square").Square
+
+
+""" Defining a Square class """
+
+
+class Square:
+    """
+    Square Class
+    """
+    # size = None
+    def __init__(self, size):
+        self._Square__size = size
+
+
+"""
+my_square = Square(3)
+print(type(my_square))
+print(my_square.__dict__)
+
+try:
+    print(my_square.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square.__size)
+except Exception as e:
+    print(e)
+"""
