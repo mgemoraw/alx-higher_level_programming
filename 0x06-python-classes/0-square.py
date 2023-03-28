01b389e37264153623706d02cc9017ae93451187
mgemoraw <meng.get4@gmail.com>
2023-03-28 22:22:49 +0300

    My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 class Square:
     """
-    Square:
-        defines square of a number
+    Square is a class which defines a square method
     """
     pass
 
