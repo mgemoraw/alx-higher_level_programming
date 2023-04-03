1bee0a864b6c4ca89a366ea2e6d3dbf01700d498
sgetme <sgetme@outlook.com>
2023-04-03 14:52:45 +0300

    Redifining Rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -18,24 +18,26 @@ class Rectangle:
 
     @property
     def width(self):
-        return self.__width
+        return self._width
 
     @width.setter
     def width(self, value):
-        if not(isinstance(self.width, int)):
+        if not(isinstance(value, int)):
             raise TypeError('width must be an integer')
-        if self.width < 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+
+        self._width = value
 
     @property
     def height(self):
-        return self.__height
+        return self._height
 
     @height.setter
     def height(self, value):
-        if not(isinstance(self.heigt, int)):
+        if not(isinstance(value, int)):
             raise TypeError('height must be an integer')
-        if self.height < 0:
+        if value < 0:
             raise ValueError('height must be >= 0')
-        self.__height = value
+
+        self._height = value
