4190b6f5dc5559b25dbebd7eae5eab5a27634780
sgetme <sgetme@outlook.com>
2023-04-03 14:48:24 +0300

    redifining recngale class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,29 +6,36 @@ class Rectangle:
     """Rectangle Class"""
 
     def __init__(self, width=0, height=0):
+        """Initialize enw Rectangle:
+
+        Args:
+            width(int): The width of the rectangle
+            height(int): The height of the rectnagle
+        """
+
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
         if not(isinstance(self.width, int)):
             raise TypeError('width must be an integer')
         if self.width < 0:
             raise ValueError('width must be >= 0')
-        return self.width
-
-    @width.setter
-    def width(self, value):
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
         if not(isinstance(self.heigt, int)):
             raise TypeError('height must be an integer')
         if self.height < 0:
             raise ValueError('height must be >= 0')
-        return self.height
-
-    @height.setter
-    def height(self, value):
-        self._height = value
+        self.__height = value
