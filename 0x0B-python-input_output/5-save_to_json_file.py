e39ac39b17bd1bf4191c6f2e71f3bf0ba364e959
sgetme <sgetme@outlook.com>
2023-04-13 09:32:49 +0300

    noting

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-writes an object to text file using JSON
-"""
+"""writes an object to text file using JSON"""
 import json
 
-def save_to_file(my_obj, filename):
-    """
-    Writes an object to text file using JSON
+def save_to_json_file(my_obj, filename):
+    """Writes an object to text file using JSON
     Args:
         my_obj (str): string object 
         filename (str): name of the file
     """
     with open(filename, "w") as file:
-        json.dumps(my_obj, file)
+        json.dump(my_obj, file)
