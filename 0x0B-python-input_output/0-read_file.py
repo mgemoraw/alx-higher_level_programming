61b19d7a19230c66a6175f4d87377a691f2da685
sgetme <sgetme@outlook.com>
2023-04-13 09:21:22 +0300

    write to a file
    :

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,3 @@ def read_file(filename=""):
     """Prints the contents of a text file (UTF8) file format into the stdout"""
     with open(filename, encoding="utf-8") as file:
         print(file.read(), end="")
-        #print()
