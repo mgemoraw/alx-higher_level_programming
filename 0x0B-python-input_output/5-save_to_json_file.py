0a71904d5653ac280c6ae0bb0f02f1d431258aef
sgetme <sgetme@outlook.com>
2023-04-13 09:25:57 +0300

    save to object file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,4 +12,4 @@ def save_to_file(my_obj, filename):
         filename (str): name of the file
     """
     with open(filename, "w") as file:
-        json.dumps(my_obj, file)
+        json.dump(my_obj, file)
