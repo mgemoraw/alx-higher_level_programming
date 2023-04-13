3a9b5ca580e741ca9396e620fb02649a8544b818
sgetme <sgetme@outlook.com>
2023-04-13 09:34:19 +0300

    noting

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -2,10 +2,11 @@
 """writes an object to text file using JSON"""
 import json
 
+
 def save_to_json_file(my_obj, filename):
     """Writes an object to text file using JSON
     Args:
-        my_obj (str): string object 
+        my_obj (str): string object
         filename (str): name of the file
     """
     with open(filename, "w") as file:
