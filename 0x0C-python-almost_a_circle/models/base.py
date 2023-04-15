691d2c2f8a6c66d7edceff4529f4905cf109354e
sgetme <sgetme@outlook.com>
2023-04-15 11:43:06 +0300

    Base Class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Base Class"""
+
+
+class Base:
+    """ Base Classs"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Base class constructor
+        Args:
+            id (int): The identity of the new Base
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+     
