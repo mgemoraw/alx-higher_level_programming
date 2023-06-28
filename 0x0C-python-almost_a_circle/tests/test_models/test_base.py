5ae8da4c6d6fad6ae0c0b66b04aa89cf5cabb87a
mgemoraw <meng.get4@gmail.com>
2023-06-28 10:23:27 +0300

    test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,30 +1,48 @@
+#!/usr/bin/python3
+"""
+testing base module
+"""
 import unittest
 from models.base import Base
 
-class BaseTestInit(unittest.TestCase):
-	def setUp(self):
-		self.base = Base()
 
-	def test_init(self):
-		self.assertEqual(self.base.id, 3)
-	
-	def test_id_1(self):
-		self.base = Base(12)
-		self.assertEqual(self.base.id, 12)
+class TestBase(unittest.TestCase):
+	"""
+	testing cases for Base class
+	"""
+	def test_create_id(self):
+		""" tests if id is created for ever initials
+		"""
+		base1 = Base()
+		base2 = Base()
+		base3 = Base()
+		base4 = Base(15)
+		base5 = Base(-3)
+		base6 = Base(2.5)
+		base7 = Base(None)
+		base8 = Base("string")
+		base9 = Base()
 
-	def test_id_2(self):
-		self.assertEqual(self.base.id, 2)
-	
-class BaseTestStaticMethod(unittest.TestCase):
-	def setUp(self):
-		self.base = Base()
+		self.assertEqual(base1.id, 1)
+		self.assertEqual(base2.id, 2)
+		self.assertEqual(base3.id, 3)
+		self.assertEqual(base4.id, 15)
+		self.assertEqual(base5.id, -3)
+		self.assertEqual(base6.id, 2.5)
+		self.assertEqual(base7.id, 4)
+		self.assertEqual(base8.id, 'string')
+		self.assertEqual(base9.id, 5)
+
 
 
 	def test_to_json_string(self):
-		self.assertEqual(self.base.to_json_string([]), '[]')
+		self.assertEqual(Base.to_json_string([]), '[]')
 
 	def test_to_json_string1(self):
-		# self.assertEqual(self.base.to_json_string(['name, 0']), '[name, 0]')
-		pass
+		self.assertEqual(Base.to_json_string(None), '[]')
+	
 	def test_from_json_string(self):
-		self.assertEqual(self.base.from_json_string('[]'), [])
+		self.assertEqual(Base.from_json_string('[]'), [])
+
+if __name__ == "__main__":
+	unittest.main()
