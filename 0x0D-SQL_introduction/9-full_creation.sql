bfc43d9e083abb7d224e5af99eda32ddd07144f5
mgemoraw <meng.get4@gmail.com>
2023-07-20 03:52:54 +0300

    My Name

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -7,5 +7,5 @@ CREATE TABLE IF NOT EXISTS `second_table`(
 
 INSERT INTO `second_table`(`id`, `name`, `score`) values(1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) values(2,"Alex", 3);
-INSERT INTO `second_table`values(3, "Bob", 14);
+INSERT INTO `second_table` values(3, "Bob", 14);
 INSERT INTO `second_table`values(4, "George", 8);
