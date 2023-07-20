f1de2ab8d38298ee55a77edd4cf334df2a3f0bd7
mgemoraw <meng.get4@gmail.com>
2023-07-20 03:47:21 +0300

    full creation

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,14 +5,7 @@ CREATE TABLE IF NOT EXISTS `second_table`(
     `score` INT
 );
 
-INSERT INTO `second_table` values(
-    `id`=1, `name`="John", `score`=10);
-INSERT INTO `second_table`
-values(
-    `id`=2, `name`="Alex", score=3);
-INSERT INTO `second_table`
-values(
-    `id`=3, `name`="Bob", `score`=14);
-INSERT INTO `second_table`
-values(
-    `id`=4, `name`="George", `score`=8);
+INSERT INTO `second_table`(`id`, `name`, `score`) values(1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) values(2,"Alex", 3);
+INSERT INTO `second_table`values(3, "Bob", 14);
+INSERT INTO `second_table`values(4, "George", 8);
