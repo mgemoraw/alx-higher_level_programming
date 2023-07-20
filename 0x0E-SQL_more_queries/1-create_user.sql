6374e8b101529b9ca0d32aff09992eb13820a5c4
mgemoraw <meng.get4@gmail.com>
2023-07-20 05:57:58 +0300

    grant previlage

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- creates the MySQL server user user_0d_1 
+-- and grant all priviledges
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+
+-- grant privilage
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
