3569f142b28002c178d8f352d2594e38ac99a585
mgemoraw <meng.get4@gmail.com>
2023-11-16 11:47:40 +0300

    An integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -5,5 +5,5 @@ const args = process.argv;
 if (!(parseInt(args[2]))) {
   console.log('Not a number');
 } else {
-  console.log(`My Number: ${parseInt(args[2])}`);
+  console.log(`My number: ${parseInt(args[2])}`);
 }
