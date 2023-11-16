26203e203277fd6171d1a08ce689193c3aa59551
mgemoraw <meng.get4@gmail.com>
2023-11-16 12:01:32 +0300

    Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const lines = ['C is fun','Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 // prints 3 lines: (like 1-multi_languages.js)
 for (const t in lines) {
