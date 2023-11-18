b48b4274e0e639b51a757a978aa8c14605dfc6a4
mgemoraw <meng.get4@gmail.com>
2023-11-18 13:20:28 +0300

    Rectangle#1

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor (w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  // exports.Rectangle = Rectangle;
+  module.exports = Rectangle;
+  
