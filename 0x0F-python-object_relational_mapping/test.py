f2d2b32eb5ab7cb931ce8f9ee9df6a26964e0d5c
mgemoraw <meng.get4@gmail.com>
2023-11-18 16:52:07 +0300

    Startup files

---
+++ 0x0F-python-object_relational_mapping/test.py
@@ -0,0 +1,21 @@
+import MySQLdb
+
+
+# Without ORM
+conn = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="my_db", charset="utf8")
+cur = conn.cursor()
+cur.execute("SELECT * FROM states ORDER BY id ASC") # HERE I have to know SQL to grab all states in my database
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+conn.close()
+
+# With ORM
+engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format("root", "root", "my_db"), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+for state in session.query(State).order_by(State.id).all(): # HERE: no SQL query, only objects!
+    print("{}: {}".format(state.id, state.name))
+session.close()
