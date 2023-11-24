326abf51c998146a6dba8c636d4f3bb1a7754cf3
mgemoraw <meng.get4@gmail.com>
2023-11-24 16:59:39 +0300

    adding all files at once

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists all states from hbtn_0e_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY '{}'""".format(sys.argv[4]))
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    cursor.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all states from hbtn_0e_usa"""
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+
+    mt = sys.argv[4]
+    cursor.execute("""SELECT * FROM states WHERE name LIKE %s""", (mt, ))
+    records = cursor.fetchall()
+    for record in records:
+        print(record)
+    cursor.close()
+    db.close()
