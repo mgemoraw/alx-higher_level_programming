11005f2ccd0f310a6c924ed6724b18beee916ed0
mgemoraw <meng.get4@gmail.com>
2023-12-01 16:55:09 +0300

    Search API

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,7 +9,6 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
 
     req = requests.post(url)
     if req.status_code >= 400:
---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    arg = "" if len(sys.argv[1]) == 1 else sys.argv[1]
+
+    payload = {'q': arg}
+    url = 'http://0.0.0.0:5000/search_user'
+    req = requests.post(url, params=payload)
+
+    try:
+        response = req.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
