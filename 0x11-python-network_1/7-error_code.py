160f7fac129add89cc790f595bdb6c88c6c6739e
mgemoraw <meng.get4@gmail.com>
2023-12-01 16:56:44 +0300

    Error #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,7 +10,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
 
-    req = requests.post(url)
+    req = requests.get(url)
     if req.status_code >= 400:
         print("Error code: {}".format(req.status_code))
     else:
