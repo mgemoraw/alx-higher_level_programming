a9b4da6f5bd711cbc6826b1e83e5e77e1e35d379
mgemoraw <meng.get4@gmail.com>
2024-01-04 17:20:44 +0300

    write web request to a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
 const fs = require('fs');
+const request = require('request');
 
 // Specifying the file path
-const filePath = process.argv[2];
-const data = process.argv[3];
+const url = process.argv[2];
+const filePath = process.argv[3];
 
-writeFile(filePath);
-
-function writeFile (filePath) {
-  // Read the content of the file asynchronously
+// getting requests
+request(url, (_err, data) => {
+  // writing result to the filepath
   fs.writeFile(filePath, data, 'utf8', (err) => {
     if (err) {
-      // Handles errors, e.g., file not found
       console.log(err);
     }
   });
-}
+});
