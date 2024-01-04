b2d515b0d7ab352518748483511fc0595a4dc251
mgemoraw <meng.get4@gmail.com>
2024-01-04 17:08:07 +0300

    Starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -6,8 +6,8 @@ const movieId = process.argv[2];
 const SWAPI = 'https://swapi-api.alx-tools.com/api/films/'.concat(movieId);
 
 // getting requests
-request(SWAPI, (_err, _data, b) => {
-  b = JSON.parse(b);
+request(SWAPI, (_err, _data, movie) => {
+  movie = JSON.parse(movie);
   // print respons status code to the console
-  console.log(b.title);
+  console.log(movie.title);
 });
