251f3a3a5aaf63726770e60ff82012c84b62670b
mgemoraw <meng.get4@gmail.com>
2024-01-04 17:03:06 +0300

    starwars movie api

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,13 +5,9 @@ const request = require('request');
 const movieId = process.argv[2];
 const  SWAPI = 'https://swapi-api.alx-tools.com/api/films/'.concat(movieId);
 
-movieTitle(SWAPI);
-
 // getting requests
-const movieTitle = (movieId) => {
-  request(SWAPI, (err, data, b) => {
-    b = JSON.parse(b);
-    // print respons status code to the console
-    console.log(b.title);
-  });
-}
+request(SWAPI, (err, data, b) => {
+  b = JSON.parse(b);
+  // print respons status code to the console
+  console.log(b.title);
+});
