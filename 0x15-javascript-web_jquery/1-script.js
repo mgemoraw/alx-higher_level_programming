e53cc7fe36267e92eaf89198eca783bf46b03830
mgemoraw <meng.get4@gmail.com>
2024-02-07 16:49:30 +0300

    using JQuery

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
-  //change color of header element to #FF0000
+$(document).ready(function () {
+  // change color of header element to #FF0000
   $('header').css('color', '#FF0000');
 });
