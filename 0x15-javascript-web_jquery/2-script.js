4e7f079f712912ecb2cb7fec3fb593ae2f9d5597
mgemoraw <meng.get4@gmail.com>
2024-02-07 16:59:46 +0300

    Clicked

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // change color of header element to #FF0000
+    $('#red_header').click(function () {
+        $('header').css('color', '#FF0000');
+    })
+  });
