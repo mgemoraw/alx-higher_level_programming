1c28bb9a0726b384aa5552d4d3d3396fba39564a
mgemoraw <meng.get4@gmail.com>
2024-02-12 16:40:50 +0300

    starwars movies

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     // change color of header element to #FF0000
     url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 
-  $.get(url), function (data) {
+  $.get(url, function (data) {
     $('DIV#character').text(data.name);
-  };
+  });
 });
