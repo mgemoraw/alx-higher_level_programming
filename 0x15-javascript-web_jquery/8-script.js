e568e289aabb155496e8e4114b088cddbe1fc5be
mgemoraw <meng.get4@gmail.com>
2024-02-12 17:02:52 +0300

    And Press ENTER

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // change color of header element to #FF0000
-    url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+    const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
 
   $.get(url, function (data) {
     $('UL#list_movies').append(...data.results.map(movie=> `<li>${movie.title}</li>`));
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     // change color of header element to #FF0000
-    url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+    const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
 
   $.get(url, function (data) {
     $('DIV#hello').text(data.hello);
